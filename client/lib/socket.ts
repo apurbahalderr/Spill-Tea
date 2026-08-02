b03c10bed02ff77f846b434b6d5a7f4b393e9277
Apurba Halder <apurbahalder.rnc@gmail.com>
2026-08-02 20:29:55 +0530

    fix: update socket connection to use environment variable for URL

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -1,5 +1,3 @@
-import { io, Socket } from "socket.io-client";
-
-export const socket: Socket = io("http://localhost:4000", {
+export const socket: Socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:4000", {
   autoConnect: false,
 });
